eb984f493a0003a4b0cf24fdcba89712a434a5b4
abdullahDew <abdullahs@dewsolutions.in>
2023-03-03 16:07:48 +0530

    Dev main yml file updated

--- src/utils/output.js
+++ src/utils/output.js
@@ -23,7 +23,7 @@ exports.setOutput = async function setOutput(resultsPath) {
   core.setOutput('assertionResults', assertionResults ? JSON.stringify(assertionResults) : '')
   // core.setOutput('manifest', manifestResults ? JSON.stringify(manifestResults) : '')
   core.setOutput('htmlPath', manifestResults ? JSON.stringify(manifestResults[0].htmlPath) : '')
-  core.setOutput('summaryData', manifestResults ? JSON.stringify(manifestResults) : '')
-  core.setOutput('jsonPath', manifestResults ? JSON.stringify(manifestResults) : '')
-  core.setOutput('actualUrl', manifestResults ? JSON.stringify(manifestResults) : '') 
+  // core.setOutput('summaryData', manifestResults ? JSON.stringify(manifestResults) : '')
+  // core.setOutput('jsonPath', manifestResults ? JSON.stringify(manifestResults) : '')
+  // core.setOutput('actualUrl', manifestResults ? JSON.stringify(manifestResults) : '') 
 }
