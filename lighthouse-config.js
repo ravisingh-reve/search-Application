77aa2a4a9a455ec92dc0f12e228fc8e9633744f4
ravisingh-reve <rkumar15690@gmail.com>
2023-03-07 16:27:09 +0530

    Update lighthouse-config.js

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -4,11 +4,11 @@ module.exports = {
     emulatedFormFactor: 'mobile',
     throttling: { rttMs: 40, throughputKbps: 10240, cpuSlowdownMultiplier: 1 },
     audits: [
-      { path: 'metrics/first-contentful-paint', options: { scorePODR: 800, scoreMedian: 1600 } },
-      { path: 'metrics/first-meaningful-paint', options: { scorePODR: 800, scoreMedian: 1600 } },
-      { path: 'metrics/speed-index', options: { scorePODR: 1100, scoreMedian: 2300 } },
-      { path: 'metrics/interactive', options: { scorePODR: 2000, scoreMedian: 4500 } },
-      { path: 'metrics/first-cpu-idle', options: { scorePODR: 2000, scoreMedian: 4500 } }
+      { path: 'metrics/first-contentful-paint', options: { scorePODR: 80000, scoreMedian: 16000 } },
+      { path: 'metrics/first-meaningful-paint', options: { scorePODR: 80000, scoreMedian: 160000 } },
+      { path: 'metrics/speed-index', options: { scorePODR: 110000, scoreMedian: 230000 } },
+      { path: 'metrics/interactive', options: { scorePODR: 200000, scoreMedian: 450000 } },
+      { path: 'metrics/first-cpu-idle', options: { scorePODR: 200000, scoreMedian: 4500000 } }
     ]
   }
 }
